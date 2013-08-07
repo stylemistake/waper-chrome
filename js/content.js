8247a2bb8fdc9d417ff343ad5fcced122c774dd7
stylemistake <aleksej@mushroomagency.com>
2013-08-07 07:58:47 +0300

    Moved assets

--- js/content.js
+++ js/content.js
@@ -2,6 +2,15 @@
 
 	$(document).ready( function() {
 
+		// Change favicon
+		(function() {
+			var link = document.createElement('link');
+			link.type = 'image/x-icon';
+			link.rel = 'shortcut icon';
+			link.href = chrome.extension.getURL("assets/icon.png");
+			document.head.appendChild(link);
+		})();
+
 		// Bind an event to all anchors with "go.php" in it.
 		$("a[href*='/go/?u=']").on( "click", function( event ) {
 			var url = $(this).attr("href").match(/\?u=(.*)/)[1];

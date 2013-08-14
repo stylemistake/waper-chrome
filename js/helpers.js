040625e2707e52d6e357ad5b3d92405b2ac0883d
stylemistake <aleksej@mushroomagency.com>
2013-08-14 07:42:07 +0300

    This debug line would make you shit in your pants.

--- js/helpers.js
+++ js/helpers.js
@@ -1,7 +1,6 @@
 var Helpers = {
 	getDateDiff: function( date1, date2 ) {
 		if ( date2 === undefined ) date2 = (new Date()).getTime();
-		console.log( date1, date2, date2 - date1 );
 		return date2 - date1;
 	}
 }
